b9df43cfef9811f693f27659bc029bceb649bdef
Armin Heravi <armin.heravi@gmail.com>
2024-11-17 19:50:43 -0500

    cleanup map2

--- plotly_map2.py
+++ plotly_map2.py
@@ -2,29 +2,26 @@
 """
 Created on Thu Nov 30 08:23:24 2023
 @author: g92269
-Description: Second iteration of mapping capability with plotly.  Using csv of world airports to plot 'large_airports'
-
-Using hard-copy json and csv files for proof of concept for closed areas.
-    
+Description: Second iteration of mapping capability with plotly.  
+Using csv of world airports to plot 'large_airports'
+Using hard-copy json and csv files for proof of concept.
 """
 # breakpoint()
 import plotly.graph_objects as go
 import pandas as pd
-# from plotly.Offline import plot
+# from plotly.OFFLINE import plot
 from dash import Dash, dcc, html, dash_table
 
-Portnumber = 8050
-
+PORTNUMBER = 8050
 # Can be passed as argument to application, automatically determined, etc.
-Offline = True
-
+OFFLINE = True
 app = Dash()
 
-serve_locally = Offline
-app.scripts.config.serve_locally = Offline
-app.css.config.serve_locally = Offline
+serve_locally = OFFLINE
+app.scripts.config.serve_locally = OFFLINE
+app.css.config.serve_locally = OFFLINE
 app.get_asset_url = '/assets'
-plotlyConfig = {'topojsonURL':'assets/topojson/'} if Offline else {}
+plotlyConfig = {'topojsonURL':'assets/topojson/'} if OFFLINE else {}
 df2 = pd.read_csv('assets/airports.csv')
 large_airports = df2[df2['type']=='large_airport']
 fig = go.Figure()
@@ -43,12 +40,10 @@ fig.add_trace(go.Scattergeo(
         color = 'blue', #'rgb(255, 0, 0)',
         line = dict(
             width = 2,
-            color = 'rgba(68, 68, 68, 0)'
-        )    
-    )))
+            color = 'rgba(68, 68, 68, 0)'))))
 fig.update_layout(
     title_text = 'Major Airports and ICAO Identifiers',
-    width=800, 
+    width=800,
     height=800,
     showlegend = False,
     geo = {
@@ -80,12 +75,11 @@ fig.update_layout(
     }
 )
 gr = dcc.Graph(figure=fig, config=plotlyConfig,)
-app.layout = html.Div([ 
-    gr,
-    dash_table.DataTable(data=large_airports.to_dict('records'), 
+app.layout = html.Div([gr,
+    dash_table.DataTable(data=large_airports.to_dict('records'),
                          page_size=50,
                          style_cell={'textAlign': 'left', 'font_size': '8px'})
     ])
 
 if __name__ == '__main__':
-    app.run(debug=False, port=Portnumber)
+    app.run(debug=False, port=PORTNUMBER)
